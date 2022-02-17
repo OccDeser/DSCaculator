5acbcf8ca827e264e68ff0c3f3c74e0e947528ba
OccDeser <2287109950@qq.com>
2022-02-17 16:15:38 +0800

    first commit

---
+++ DysonSphereCaculator.py
@@ -0,0 +1,193 @@
+import os
+import json
+from math import gcd
+from fractions import Fraction
+import re
+
+all_products = {}
+all_machines = {}
+
+
+class DSProduct(object):
+    name = ''
+    demands = {}
+    timeconsume = 0
+    outputnum = 0
+
+    def __init__(self, name, config) -> None:
+        self.name = name
+        self.loadConfig(config)
+
+    def __repr__(self) -> str:
+        return self.name
+
+    def loadConfig(self, config):
+        f = open(config, 'r', encoding='utf8')
+        data = json.load(f)
+        self.demands = data[0]
+        self.timeconsume = data[1]
+        self.outputnum = data[2]
+        f.close()
+
+    def need(self, number, time):
+        if self.demands['machine'] == '矿机':
+            return 1, '矿机', []
+        machine = all_machines[self.demands['machine']]
+        mnumber = number * self.timeconsume / time / machine.efficient
+        products = []
+        for p in self.demands['products']:
+            product = all_products[p]
+            number = self.demands['products'][p]
+            number = number / self.outputnum
+            products.append({
+                'product': product,
+                'number': number
+            })
+        return mnumber, machine, products
+
+
+class DSMachine(DSProduct):
+    name = ''
+    demands = {}
+    efficient = 1
+    timeconsume = 0
+    outputnum = 0
+
+    def loadConfig(self, config):
+        f = open(config, 'r', encoding='utf8')
+        data = json.load(f)
+        self.demands = data[0]
+        self.timeconsume = data[1]
+        self.outputnum = data[2]
+        self.efficient = data[3]
+        f.close()
+
+
+class DSTreeNode(object):
+    def __init__(self, product, pnumber=1, ptime=0) -> None:
+        self.name = product.name
+        self.children = []
+        self.machine = []
+        self.pnumber = pnumber
+        if ptime == 0:
+            machine = all_machines[product.demands['machine']]
+            ptime = product.timeconsume / machine.efficient
+        self.getDemands(product, pnumber, ptime)
+
+    def __repr__(self) -> str:
+        return self.name
+
+    def getDemands(self, product, pnumber, ptime):
+        mnumber, machine, children = product.need(pnumber, ptime)
+        self.machine = machine
+        self.mnumber = float(mnumber)
+        for child in children:
+            childnum = child['number'] * pnumber / product.outputnum
+            childnode = DSTreeNode(child['product'], childnum, ptime)
+            self.children.append(childnode)
+
+    def formatTree(self, prelist=[], depth=0):
+        res = ''
+        if depth == 0:
+            res += '%s: %d\n' % (self.name, self.pnumber)
+        if len(self.children) != 0:
+            for p in prelist:
+                if p:
+                    res += '│   '
+                else:
+                    res += '    '
+            res += "├── %s: %.0f\n" % (self.machine, self.mnumber)
+
+        for childnode in self.children:
+            for p in prelist:
+                if p:
+                    res += '│   '
+                else:
+                    res += '    '
+            if childnode != self.children[-1]:
+                res += '├── %s: %.0f\n' % (childnode.name, childnode.pnumber)
+                prelist.append(1)
+            else:
+                res += '└── %s: %.0f\n' % (childnode.name, childnode.pnumber)
+                prelist.append(0)
+            res += childnode.formatTree(prelist, depth+1)
+            prelist = prelist[:depth]
+        return res
+
+    def printTree(self, prelist=[], depth=0):
+        print(self.formatTree(), end='')
+
+
+class DSCaculator(object):
+    def __init__(self, configdir) -> None:
+        self.denominators = []
+        self.statistics = {}
+        f = open(os.path.join(configdir, 'config.json'), 'r', encoding='utf8')
+        data = json.load(f)
+        f.close()
+
+        pdir = os.path.join(configdir, 'products')
+        products = data['products']
+        for p in products:
+            all_products[p] = DSProduct(p, os.path.join(pdir, p+'.json'))
+
+        mdir = os.path.join(configdir, 'machines')
+        machines = data['machines']
+        for m in machines:
+            all_machines[m] = DSMachine(m, os.path.join(mdir, m+'.json'))
+    
+    def clear(self):
+        self.denominators.clear()
+        self.statistics.clear()
+
+    def getProductDenominators(self, dstree):
+        for child in dstree.children:
+            f = Fraction(child.pnumber)
+            self.denominators.append(f.limit_denominator().denominator)
+            self.getProductDenominators(child)
+
+    def countMachines(self, dstree):
+        if dstree.machine == "矿机":
+            return
+        produce_type = (dstree.machine.name, dstree.name)
+        if dstree in self.statistics:
+            self.statistics[produce_type] += dstree.mnumber
+        else:
+            self.statistics[produce_type] = dstree.mnumber
+        for c in dstree.children:
+            self.countMachines(c)
+
+    def getDenominatorsLCM(self, dstree):
+        self.getProductDenominators(dstree)
+        self.countMachines(dstree)
+        for s in self.statistics:
+            f = Fraction(self.statistics[s])
+            self.denominators.append(f.limit_denominator().denominator)
+        lcm = 1
+        for d in self.denominators:
+            lcm = int(lcm*d/gcd(lcm, d))
+        return lcm
+
+    def formatStatis(self):
+        res = ''
+        for s in sorted(self.statistics.items(), key=lambda x: x[0], reverse=False):
+            res += "%s %s %d\n" % (s[0][0], s[0][1], s[1])
+        return res
+
+    def printStatis(self):
+        for s in sorted(self.statistics.items(), key=lambda x: x[0], reverse=False):
+            print("%s %s %d" % (s[0][0], s[0][1], s[1]))
+
+    def caculate(self, product):
+        self.clear()
+        
+        if product in all_products:
+            product = all_products[product]
+        else:
+            product = all_machines[product]
+
+        resultree = DSTreeNode(product)
+        pnumber = self.getDenominatorsLCM(resultree)
+        resultree = DSTreeNode(product, pnumber)
+
+        return resultree
---
+++ DysonSphereCcauUI.py
@@ -0,0 +1,134 @@
+from tkinter import *
+from tkinter import ttk
+import DysonSphereCaculator as DSC
+
+LOG_LINE_NUM = 0
+
+
+class DSC_GUI():
+    def __init__(self, init_window_name):
+        self.init_window_name = init_window_name
+        self.dsc = DSC.DSCaculator('./config')
+
+    def set_init_window(self):
+        self.init_window_name.title("戴森球计划计算器_v0.1")
+        self.init_window_name.geometry('518x726+10+10')
+        # 标签
+        self.init_data_label = Label(self.init_window_name, text="选择产物")
+        self.init_data_label.grid(row=0, column=0)
+        # 下拉框
+        productVar = StringVar()
+        productValue = (
+            "电磁矩阵",
+            "能量矩阵",
+            "结构矩阵",
+            "信息矩阵",
+            "引力矩阵",
+            "宇宙矩阵",
+            "电弧熔炉",
+
+            "铁块",
+            "铜块",
+            "高纯硅块",
+            "钛块",
+            "石材",
+            "高能石墨",
+            "磁铁",
+            "钢材",
+            "晶格硅",
+            "钛合金",
+            "玻璃",
+            "金刚石",
+            "磁线圈",
+            "液氢燃料棒",
+            "氘核燃料棒",
+            "反物质燃料棒",
+            "电动机",
+            "钛化玻璃",
+            "棱镜",
+            "有机晶体",
+            "钛晶石",
+            "推进器",
+            "加力推进器",
+            "齿轮",
+            "电磁涡轮",
+            "电路板",
+            "引力透镜",
+            "位面过滤器",
+            "物流运输机",
+            "星际物流运输船",
+            "小型运载火箭",
+            "电浆激发器",
+            "超级磁场环",
+            "粒子宽带",
+            "处理器",
+            "卡西米尔晶体",
+            "粒子容器",
+            "空间翘曲器",
+            "湮灭约束球",
+            "太阳帆",
+            "框架材料",
+            "戴森球组件",
+            "光子合并器",
+            "微晶原件",
+            "量子芯片",
+            "空间翘曲器",
+            "地基",
+            "塑料",
+            "有机晶体",
+            "石墨烯",
+            "碳纳米管",
+            "奇异物质",
+            "精炼油",
+
+            "化工厂",
+            "粒子对撞机",
+            "原油精炼厂",
+            "制造台",
+            "矩阵研究站",
+            "矿机")
+        self.product_ComboBox = ttk.Combobox(
+            self.init_window_name, textvariable=productVar, value=productValue)
+        self.product_ComboBox.grid(row=1, column=0, rowspan=1, columnspan=5)
+
+        # 按钮
+        self.str_trans_to_md5_button = Button(
+            self.init_window_name, text="计算", bg="lightblue", width=10, command=self.caculateProduct)  # 调用内部方法  加()为直接调用
+        self.str_trans_to_md5_button.grid(row=1, column=6)
+
+        # 标签
+        self.result_data_label = Label(self.init_window_name, text="输出结果")
+        self.result_data_label.grid(row=2, column=0)
+        # 文本框
+        self.result_data_Text = Text(
+            self.init_window_name, width=70, height=49)  # 处理结果展示
+        self.result_data_Text.grid(
+            row=3, column=0, rowspan=15, columnspan=10, padx=10)
+
+    # 功能函数
+    def caculateProduct(self):
+        product = self.product_ComboBox.get()
+        self.result_data_Text.delete(1.0, END)
+        if product:
+            try:
+                result = self.dsc.caculate(product)
+                resultTree = result.formatTree(prelist=[])
+                self.dsc.countMachines(result)
+                statisData = self.dsc.formatStatis()
+                resultData = resultTree + '\n' + statisData
+                self.result_data_Text.insert(1.0, resultData)
+                print(resultData)
+            except:
+                self.result_data_Text.insert(1.0, "产物计算失败")
+        else:
+            self.result_data_Text.insert(1.0, "产物计算失败")
+
+
+if __name__ == '__main__':
+
+    init_window = Tk()  # 实例化出一个父窗口
+    ZMJ_PORTAL = DSC_GUI(init_window)
+    # 设置根窗口默认属性
+    ZMJ_PORTAL.set_init_window()
+
+    init_window.mainloop()  # 父窗口进入事件循环，可以理解为保持窗口运行，否则界面不展示
